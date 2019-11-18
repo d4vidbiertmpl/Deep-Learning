008bda55f1cb211b3fb8073f79176e31de7bafef
David Biertimpel <david.biertimpel@protonmail.com>
2019-11-18 14:10:24 +0100

    implemented LSTM

--- assignment_2/part1/lstm.py
+++ assignment_2/part1/lstm.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 
+
 ################################################################################
 
 class LSTM(nn.Module):
@@ -29,6 +30,42 @@ class LSTM(nn.Module):
         super(LSTM, self).__init__()
         # Initialization here ...
 
+        self.seq_length = seq_length
+        self.input_dim = input_dim
+        self.num_hidden = num_hidden
+        self.num_classes = num_classes
+        self.device = device
+
+        # Makes more sense than ParameterList in this case
+        self.lstm_cell = nn.ParameterDict()
+        for layer in ["g{}", "i{}", "f{}", "o{}", "p{}"]:
+            if layer == "p{}":
+                self.lstm_cell[layer.format('h')] = nn.Parameter(torch.empty(num_hidden, num_classes),
+                                                                 requires_grad=True)
+                self.lstm_cell[layer.format('b')] = nn.Parameter(torch.zeros(1, num_classes), requires_grad=True)
+            else:
+                self.lstm_cell[layer.format('x')] = nn.Parameter(torch.empty(input_dim, num_hidden), requires_grad=True)
+                self.lstm_cell[layer.format('h')] = nn.Parameter(torch.empty(num_hidden, num_hidden),
+                                                                 requires_grad=True)
+                self.lstm_cell[layer.format('b')] = nn.Parameter(torch.zeros(1, num_hidden), requires_grad=True)
+
+        for key in self.lstm_cell:
+            if not key[-1] == 'b':
+                nn.init.normal_(self.lstm_cell[key], mean=0, std=0.02)
+
+        self.h_init = torch.zeros(1, num_hidden).to(device)
+        self.c_init = torch.zeros(1, num_hidden).to(device)
+
     def forward(self, x):
         # Implementation here ...
-        pass
+        _h, _c = self.h_init, self.c_init
+        for t in range(self.seq_length):
+            _g = (x[:, t, None] @ self.lstm_cell["gx"] + _h @ self.lstm_cell["gh"] + self.lstm_cell["gb"]).tanh()
+            _i = (x[:, t, None] @ self.lstm_cell["ix"] + _h @ self.lstm_cell["ih"] + self.lstm_cell["ib"]).sigmoid()
+            _f = (x[:, t, None] @ self.lstm_cell["fx"] + _h @ self.lstm_cell["fh"] + self.lstm_cell["fb"]).sigmoid()
+            _o = (x[:, t, None] @ self.lstm_cell["ox"] + _h @ self.lstm_cell["oh"] + self.lstm_cell["ob"]).sigmoid()
+
+            _c = _g * _i + _c * _f
+            _h = _c.tanh() * _o
+
+        return _h @ self.lstm_cell["ph"] + self.lstm_cell["pb"]
---
+++ assignment_2/start.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+
+from part1 import train
+
+if __name__ == "__main__":
+    # Parse training configuration
+    parser = argparse.ArgumentParser()
+
+    # Model params
+    parser.add_argument('--model_type', type=str, default="RNN", help="Model type, should be 'RNN' or 'LSTM'")
+    parser.add_argument('--input_length', type=int, default=10, help='Length of an input sequence')
+    parser.add_argument('--input_dim', type=int, default=1, help='Dimensionality of input sequence')
+    parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
+    parser.add_argument('--num_hidden', type=int, default=128, help='Number of hidden units in the model')
+    parser.add_argument('--batch_size', type=int, default=128, help='Number of examples to process in a batch')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--train_steps', type=int, default=10000, help='Number of training steps')
+    parser.add_argument('--max_norm', type=float, default=10.0)
+    parser.add_argument('--device', type=str, default="cuda:0", help="Training device 'cpu' or 'cuda:0'")
+
+    config = parser.parse_args()
+
+    # Train the model
+    train.train(config)
